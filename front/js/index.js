43a2008b4e39f125903cbc1aef26eb498d11d57b
Eduar Fabricio Anaya Ramirez <eduar.anaya@hotmail.com>
2023-12-15 12:18:44 -0500

    Add logic api rest

--- front/js/index.js
+++ front/js/index.js
@@ -1,16 +1,41 @@
 let forn = document.getElementById("subForm");
 
-forn.addEventListener("submit", (e) => {
+forn.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     let numA = document.getElementById("numA").value;
     let numB = document.getElementById("numB").value;
+    let opt = document.getElementById("option").value;
     let total = document.getElementById("total");
 
     if (numA != "" && numB != "") {
-        console.log('Ok');
-        total.innerText = Number(numA) + Number(numB);
+        await callApiCalculator(numA, numB, opt, total);
     } else {
         total.innerText = '';
     }
 });
+
+async function callApiCalculator(numA, numB, opt, total) {
+    const payLoad = {
+        var_1: numA,
+        var_2: numB,
+        operation: opt
+    };
+
+    await fetch('http://.......', {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payLoad)
+    })
+        .then(response => response.json())
+        .then(data => {
+            total.innerText = data.result;
+        })
+        .catch(() => {
+            alert("Error, intente nuevamente en unos minutos.");
+        });
+}
